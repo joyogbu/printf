5c45165f47b77cc6c0803924e2b7368dcdbfe118
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-10-18 21:46:16 -0700

    task correction

--- _print_S.c
+++ _print_S.c
@@ -18,7 +18,7 @@ int _print_S(va_list S)
 		str = "(null)";
 	for (; str[i]; i++)
 	{
-		if (str[i] < 32 || str[i] >= 127)
+		if ((str[i] < 32 && str[i] > 0) || str[i] >= 127)
 		{
 			_putchar('\\');
 			_putchar('x');
